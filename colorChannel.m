5b630259fe887fc5410daa64b8807000f8cea402
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2024-10-08 20:00:13 -0400

    Initial Commit
    This will change all the images to it's RGB Channels

---
+++ colorChannel.m
@@ -0,0 +1,18 @@
+
+function [y] = myfun(data)
+    
+    for x = 1:30
+        image1R = str(x);
+        image1G = image1(:,:,2);
+        image1B = image1(:,:,3);
+        RegRowSt = 500;
+        RegRowFin = 900;
+        RegColSt = 500;
+        RegColFin = 700;
+        
+        %getting the rgb colour channel
+        image1GF = double(image1G);
+        image1RF = double(image1R);
+        image1BF = double(image1B);
+
+end
