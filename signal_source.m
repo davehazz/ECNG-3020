ee12592b18d3b3b8c010943cd80877e8a355b7c7
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2024-11-21 16:28:02 -0400

    Finds the mean for 200 signals
    changed to 200 frames

---
+++ signal_source.m
@@ -0,0 +1,17 @@
+function [R_Source,G_Source,B_Source] = signal_source(inputArg1,inputArg2,inputArg3)
+%UNTITLED3 Summary of this function goes here
+
+%   Detailed explanation goes here
+%this function creates the signal source from finding the mean pixel
+%intensities of each frame
+
+R_Source = zeros(1,200);
+G_Source = zeros(1,200);
+B_Source = zeros(1,200);
+
+for x = 1:200
+    R_Source(x) = mean2(inputArg1(:,:,x));
+    G_Source(x) = mean2(inputArg1(:,:,x));
+    B_Source(x) = mean2(inputArg1(:,:,x));
+
+end
