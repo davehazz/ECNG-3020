be3d06ddee3ea4311a1897819fca28d8d3cdfb69
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-01-24 00:45:38 -0400

    update

--- filter_img_light.m
+++ filter_img_light.m
@@ -6,14 +6,18 @@ function [R_filtered_light,G_filtered_light,B_filtered_light] = filter_img_light
 
 
 %this function performs the filering on the image
-R_filtered_light = zeros(401,201,200);
-G_filtered_light = zeros(401,201,200);
-B_filtered_light = zeros(401,201,200);
+R_filtered_light = zeros(246,301,161);
+G_filtered_light = zeros(246,301,161);
+B_filtered_light = zeros(246,301,161);
+
+
+[ROI_R,ROI_G,ROI_B] = region_of_interest(vectorRchannel,vectorGchannel,vectorBchannel)
+
 
 [R_Source,G_Source,B_Source] = signal_source(R_filtered_avg,G_filtered_avg,B_filtered_avg);
 
 
-for x = 1:200
+for x = 1:c
     
     R_filtered_light(x) = convn(inputArg1(:,:,x),AvgFil,'same');
     G_filtered_light(x) = convn(inputArg1(:,:,x),AvgFil,'same');
