9755c209049beba54972050dd6b168f402de26e6
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-01-24 00:43:00 -0400

    update it
    the total ROI will have 161 frames

--- region_of_interest.m
+++ region_of_interest.m
@@ -3,17 +3,17 @@ a = 151;
 b = 401;
 c = 161;
 
-        RegRowSt = 500;
-        RegRowFin = 900;
-        RegColSt = 500;
-        RegColFin = 700;
+ROI_R = zeros(a,b,c);
+ROI_G = zeros(a,b,c);
+ROI_B = zeros(a,b,c);
 
         RegRowSt = 450;
         RegRowFin = 600;
         RegColSt = 400;
         RegColFin = 800;
 
-    for x = 1:200
+
+    for x = 1:161
         ROI_R(:,:,x) = vectorRchannel(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
         ROI_G(:,:,x) = vectorGchannel(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
         ROI_B(:,:,x) = vectorBchannel(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
