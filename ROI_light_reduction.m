d21c8fad5f9a158572c78abf4c77789693bcd9f5
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-02-04 21:50:21 -0400

    update
    just changed the size of the loop and I picked an area to sample from.
    
    This is basically to subtract from the mean pixel intensity.

--- ROI_light_reduction.m
+++ ROI_light_reduction.m
@@ -3,21 +3,21 @@ function [ROI_Light_filter_R,ROI_Light_filter_G,ROI_Light_filter_B] = ROI_light_
 %   this selects the region of interest for the background light reduction
 
 
-a = 101;
-b = 101;
+a = 151;
+b = 201;
 c = 161;
 
 ROI_Light_filter_R = zeros(a,b,c);
 ROI_Light_filter_G = zeros(a,b,c);
 ROI_Light_filter_B = zeros(a,b,c);
 
-        RegRowSt = 800;
-        RegRowFin = 900;
-        RegColSt = 500;
-        RegColFin = 600;
+        RegRowSt = 500;
+        RegRowFin = 650;
+        RegColSt = 600;
+        RegColFin = 800;
 
 
-    for x = 1:161
+    for x = 1:700
         ROI_Light_filter_R(:,:,x) = inputArg1(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
         ROI_Light_filter_G(:,:,x) = inputArg2(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
         ROI_Light_filter_B(:,:,x) = inputArg3(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
