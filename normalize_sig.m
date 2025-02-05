015b409d87ff1d4fee61838b7977d10b8f3f0d80
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-02-04 21:49:07 -0400

    new function to normalize the signal

---
+++ normalize_sig.m
@@ -0,0 +1,17 @@
+function [outputArg1,outputArg2,outputArg3] = normalize_sig(inputArg1,inputArg2,inputArg3)
+%UNTITLED Summary of this function goes here
+%   this function provides the normilization of the source signal
+% it normalize = ((Sx - mean) / standard deviation )
+
+c = 700;
+
+outputArg1 = zeros(1,c);
+outputArg2 = zeros(1,c);
+outputArg3 = zeros(1,c);
+
+for start = 1:c
+    outputArg1(start) = ( (inputArg1(start) - mean(inputArg1)) / std(inputArg1));
+    outputArg2(start) = ( (inputArg2(start) - mean(inputArg2)) / std(inputArg2));
+    outputArg3(start) = ( (inputArg3(start) - mean(inputArg3)) / std(inputArg3));
+
+end
