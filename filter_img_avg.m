4e38626b47d4ac8992607660fee799647a3f1837
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-02-04 21:35:28 -0400

    update filter
    I changed the size of the vector, ie the amount of frames to process.

--- filter_img_avg.m
+++ filter_img_avg.m
@@ -1,9 +1,9 @@
 function [R_filtered_avg,G_filtered_avg,B_filtered_avg] = filter_img_avg(inputArg1,inputArg2,inputArg3)
 
 %this function performs the filering on the image
-R_filtered_avg = zeros(151,401,161);
-G_filtered_avg = zeros(151,401,161);
-B_filtered_avg = zeros(151,401,161);
+R_filtered_avg = zeros(151,301,800);
+G_filtered_avg = zeros(151,301,800);
+B_filtered_avg = zeros(151,301,800);
 
 
 %%% Averaging Filter
@@ -19,7 +19,7 @@ AvgFil = AvgFil/sum(AvgFil(:));
 %ImgG = convn(double(ROI),AvgFil,'same');
 %ImgB = convn(double(ROI3),AvgFil,'same');
 
-for x = 1:161
+for x = 1:700
     R_filtered_avg(:,:,x) = convn(inputArg1(:,:,x),AvgFil,"same");
     G_filtered_avg(:,:,x) = convn(inputArg2(:,:,x),AvgFil,"same");
     B_filtered_avg(:,:,x) = convn(inputArg3(:,:,x),AvgFil,"same");
