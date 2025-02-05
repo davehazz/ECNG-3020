a164d2a1ab45b420f28b249b4bfda08e51a3ba06
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-02-04 21:53:11 -0400

    update
    I changed the region of interest

--- region_of_interest.m
+++ region_of_interest.m
@@ -1,19 +1,19 @@
 function [ROI_R,ROI_G,ROI_B] = region_of_interest(vectorRchannel,vectorGchannel,vectorBchannel)
 a = 151;
-b = 401;
-c = 161;
+b = 301;
+c = 700;
 
 ROI_R = zeros(a,b,c);
 ROI_G = zeros(a,b,c);
 ROI_B = zeros(a,b,c);
 
-        RegRowSt = 450;
-        RegRowFin = 600;
-        RegColSt = 400;
-        RegColFin = 800;
+        RegRowSt = 500;
+        RegRowFin = 650;
+        RegColSt = 300;
+        RegColFin = 600;
 
 
-    for x = 1:161
+    for x = 1:700
         ROI_R(:,:,x) = vectorRchannel(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
         ROI_G(:,:,x) = vectorGchannel(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
         ROI_B(:,:,x) = vectorBchannel(RegRowSt:RegRowFin,RegColSt:RegColFin,x);
@@ -21,5 +21,6 @@ ROI_B = zeros(a,b,c);
         %getting the rgb colour channel
 
     end
+%figure(2);imshow(uint8(ROI_G(:,:,1)));
 
 end
