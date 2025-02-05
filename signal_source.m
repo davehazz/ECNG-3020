3559894ffb551ebe4d4ef1a5debd2911977bfe53
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-02-04 21:52:42 -0400

    update signal source
    added a variable to control the size

--- signal_source.m
+++ signal_source.m
@@ -5,11 +5,13 @@ function [R_Source,G_Source,B_Source] = signal_source(inputArg1,inputArg2,inputA
 %this function creates the signal source from finding the mean pixel
 %intensities of each frame
 
-R_Source = zeros(1,161);
-G_Source = zeros(1,161);
-B_Source = zeros(1,161);
+size = 700;
 
-for x = 1:161
+R_Source = zeros(1,size);
+G_Source = zeros(1,size);
+B_Source = zeros(1,size);
+
+for x = 1:size
     R_Source(x) = mean2(inputArg1(:,:,x));
     G_Source(x) = mean2(inputArg1(:,:,x));
     B_Source(x) = mean2(inputArg1(:,:,x));
