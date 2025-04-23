2768637d4654c4c40ec1a576b00e3e3e9f654ffd
Joshua Hazzard <71677783+davehazz@users.noreply.github.com>
2025-04-22 21:50:27 -0400

    Final Updates
    beta

--- heart_rate.m
+++ heart_rate.m
@@ -1,21 +1,30 @@
 %creating a video reader object
-v = VideoReader("C:\Users\unity\Documents\Engineering uWI\YEAR THREE\ECNG 3020\project code\videos\IMG_9901.MOV");
+v = VideoReader("C:\Users\unity\Documents\Engineering uWI\YEAR THREE\ECNG 3020\project code\videos\IMG_9988.MOV");
+
 
 videor = read(v, [1 600]);
 imved = v.NumFrames;
 
-imshow(readFrame(v));
 
 te(:,:,1) = double(videor(:,:,1,1)); %aquire red channel
 t = 0:1/29.98:20;
 %-------------Part 1:vectors for each color channel---------
 
-[vectorRchannel,vectorGchannel,vectorBchannel,imgg] = color_channel(v);
+%-- forget this --> [vectorRchannel,vectorGchannel,vectorBchannel,imgg] = color_channel(v);
+
+%--- run from here 
 len = 600;
 vectorRchannel = zeros(1088,1920,len);
 vectorGchannel = zeros(1088,1920,len);
 vectorBchannel = zeros(1088,1920,len);
+%--- run to here 
 
+currAxes = axes;
+vidFrame = readFrame(v);
+image(vidFrame,"Parent",currAxes);
+
+
+%--- run this line
 for x =1:600
     
     vectorRchannel(:,:,x) = double(videor(:,:,1,x)); %aquire red channel
@@ -23,10 +32,11 @@ for x =1:600
     vectorBchannel(:,:,x) = double(videor(:,:,3,x)); %aquire blue channel
 
 end 
+%--- run this line
 
 
 figure("Name","whole image");
-imshow(uint8(videor(:,:,2)));
+imshow(videor(:,:,2));
 
 %figure(1);imshow(uint8(vectorRchannel(:,:,1)));
 figure('Name','red channel');imshow(uint8(vectorRchannel(:,:,1)));
@@ -37,34 +47,56 @@ figure('Name','blue chennel');imshow(uint8(vectorBchannel(:,:,1)));
 
 %------------Part 2: Region of Interest--------------
 
-[ROI_R,ROI_G,ROI_B] = region_of_interest(vectorRchannel,vectorGchannel,vectorBchannel);
+vectorRchannel = 0;
+vectorGchannel = 0;
+vectorBchannel = 0;
+[ROI_R,ROI_G,ROI_B] = region_of_interest(vectorRchannel,vectorGchannel,vectorBchannel); %--- run this line
+
+figure(4);imshow(uint8(ROI_G(:,:,10))); %--- run this line
 
 
+[R_Source_m2,G_Source_m2,B_Source_m2] = signal_source(ROI_R,ROI_G,ROI_B); %--- run this line
 
-[R_Source_m2,G_Source_m2,B_Source_m2] = signal_source(ROI_R,ROI_G,ROI_B);
+%{
 R_m2_1 = zeros(1,600);
 G_m2_2 = zeros(1,600);
 B_m2_3 = zeros(1,600);
 
 for x = 1:600
-    R_m2_1(:,x) = (G_Source(:,x) ./ R_Source(:,x)) -1;
-    G_m2_2(:,x) = (G_Source(:,x) ./ B_Source(:,x)) -1;
-    B_m2_3(:,x) = (B_Source(:,x) ./ R_Source(:,x)) -1;
+    R_m2_1(:,x) = (G_Source_m2(:,x) ./ R_Source_m2(:,x)) -1;
+    G_m2_2(:,x) = (G_Source_m2(:,x) ./ B_Source_m2(:,x)) -1;
+    B_m2_3(:,x) = (B_Source_m2(:,x) ./ R_Source_m2(:,x)) -1;
 
 end
 
-plot(t,R_m2_1);
+figure('Name','ratio method');
+plot(t,R_m2_1,Color='r');
+hold on
+plot(t,G_m2_2,Color='g');
+plot(t,B_m2_3,Color='b');
+hold off
+%}
 
 
-figure(4);imshow(uint8(ROI_G(:,:,10)));
+R_m2_1 = 0;
+G_m2_2 = 0;
+B_m2_3 = 0;
+
+
 figure(5);imshow(uint8(ROI_G(:,:,60)));
 figure(6);imshow(uint8(ROI_G(:,:,90)));
+
 
 brk = uint8(ROI_G(:,:,10));
 brk = brk./3;
 imshow(brk);
+
+ROI_R = 0;
+ROI_G = 0;
+ROI_B = 0;
+
 %------------Part 3:filtering the image----------------
-[R_filtered,G_filtered,B_filtered] = filter_img_avg(ROI_R,ROI_G,ROI_B);
+[R_filtered,G_filtered,B_filtered] = filter_img_avg(ROI_R,ROI_G,ROI_B); %--- run this line
 
 figure(7);imshow(uint8(G_filtered(:,:,10))); %at 10
 figure(8);imshow(uint8(G_filtered(:,:,10)));% at 5
@@ -72,45 +104,65 @@ figure(9);imshow(uint8(G_filtered(:,:,10)));% at 0
 
 
 %background light reduction
-[ROI_Light_filter_R,ROI_Light_filter_G,ROI_Light_filter_B] = ROI_light_reduction(vectorRchannel,vectorGchannel,vectorBchannel);
-figure(10);imshow(uint8(ROI_Light_filter_G(:,:,60)));
+[ROI_Light_filter_R,ROI_Light_filter_G,ROI_Light_filter_B] = ROI_light_reduction(vectorRchannel,vectorGchannel,vectorBchannel); %--- run this line   
+figure(10);imshow(uint8(ROI_Light_filter_G(:,:,10)));
 
-[RC_filtered,GC_filtered,BC_filtered] = filter_img_avg(ROI_Light_filter_R,ROI_Light_filter_G,ROI_Light_filter_B);
+[RC_filtered,GC_filtered,BC_filtered] = filter_img_avg(ROI_Light_filter_R,ROI_Light_filter_G,ROI_Light_filter_B);%--- run this line
+figure(9);imshow(uint8(GC_filtered(:,:,10)));% at 0
 
 
 %------------Part 4: Signal Source------------------
-[R_Source,G_Source,B_Source] = signal_source(R_filtered,G_filtered,B_filtered);
+[R_Source,G_Source,B_Source] = signal_source(R_filtered,G_filtered,B_filtered);%--- run this line
 %[R_Source,G_Source,B_Source] = signal_source(ROI_R,ROI_G,ROI_B);
 
 figure('Name','after bk reduction')
-imshow(uint8(G_filtered(:,:,10)));
+imshow(uint8(G_filtered(:,:,90)));
 
-[R_Source_light,G_Source_light,B_Source_light] = signal_source(RC_filtered,GC_filtered,BC_filtered);
+[R_Source_light,G_Source_light,B_Source_light] = signal_source(RC_filtered,GC_filtered,BC_filtered);%--- run this line
 %without any moving average filter
 %[R_Source_light,G_Source_light,B_Source_light] = signal_source(ROI_Light_filter_R,ROI_Light_filter_G,ROI_Light_filter_B);
 j = medfilt2(RC_filtered,[3 3]);
 i = median(RC_filtered,1);
-%light reduction
+
+%----run below lines
 R_c = zeros(1,600);
 G_c = zeros(1,600);
 B_c = zeros(1,600);
+%--- run this line
 
 
+%--- run this line
 for x = 1:600
     R_c(:,x) = R_Source(:,x) - 0.75*R_Source_light(:,x);
     G_c(:,x) = G_Source(:,x) - 0.75*G_Source_light(:,x);
     B_c(:,x) = B_Source(:,x) - 0.75*B_Source_light(:,x);
 
 end
+%---------till here
 
+%{
 figure(11);
-plot(t,R_c,Color="r",Marker=".");
+plot(t,R_c,Color="r");
 hold on
 %figure(5);
-plot(t,G_c,Color="g",Marker=".");
+plot(t,G_c,Color="g");
 %figure(6);
-plot(t,B_c,Color="b",Marker=".");
+plot(t,B_c,Color="b");
 hold off
+%}
+
+%---------MEAN vs MEDIAN Pixel Intensity--------
+
+%{
+G_c_mean = G_c;
+G_c_median;
+
+plot(t,G_c_mean);
+hold on
+plot(t,G_c_median);
+legend('Mean  Data','Median  Data')
+hold off
+
 
 
 figure('Name','source signals');
@@ -122,29 +174,41 @@ plot(t,G_Source_light,Color="g",Marker="^");
 plot(t,B_Source_light,Color="b",Marker="o");
 hold off
 
+%}
+
+
 
 
 %------------Part 5: Post Signal Filtering------------------
  
 %Normalization
-[normalizeR,normalizeG,normalizeB] = normalize_sig(R_c,G_c,B_c);
+[normalizeR,normalizeG,normalizeB] = normalize_sig(R_c,G_c,B_c); %--- run this line
+
+%{
+R_c = 0;
+G_c = 0;
+B_c = 0;
 
 newR = normalize(R_Source);
 newG = normalize(G_Source);
 newB = normalize(B_Source);
-figure(13);plot(t,newG);
+%my normalize was not within the range of -2 to +2
+figure('Name','after normalization');
+plot(t,newG);
 hold on
-figure(14);
 plot(t,normalizeG);
+legend('Matlab Data','My Data')
 hold off
+
 
 
 %before ICA
 figure('Name','After Normalization');
-plot(t,normalizeR,LineStyle="-");
+plot(t,normalizeR,Color="r");
 hold on
+figure('Name','After Normalization');
 plot(t,normalizeG,Color='g');
-plot(t,normalizeB,LineStyle=":");
+plot(t,normalizeB,Color="b");
 hold off
 
 figure('Name','before detreding');
@@ -154,23 +218,34 @@ plot(t,newG,LineStyle=":");
 legend('MINE  Data','MATLAB  Data')
 hold off
 
+%}
+
+
+
 
 %Detrending
 
-R_de = zeros(1,700);
-G_de = zeros(1,700);
-B_de = zeros(1,700);
 
+%-------run below lines
 R_de = detrend(normalizeR);
 G_de = detrend(normalizeG);
 B_de = detrend(normalizeB);
+%-------till here
+
 
 %matlab normalize
+%{
+
 RR_de = detrend(newR);
 GG_de = detrend(newG);
 BB_de = detrend(newB);
 
-figure(16);
+RR_de = 0;
+GG_de = 0;
+BB_de = 0;
+
+
+figure('Name','detrending');
 plot(t,G_de,LineStyle="-");
 hold on
 plot(t,GG_de,LineStyle=":");
@@ -184,9 +259,13 @@ plot(t,G_de);
 plot(t,B_de);
 hold off
 
+%}
+
 %-------------more moving average filtering--------
 
 
+%------------- STEP NOT NEEDED-------------
+%{
 windowSize = 7; %setting filter settings
 b = (1/windowSize)*ones(1,windowSize);
 a = 1;
@@ -204,6 +283,7 @@ legend('R Channel','G channel')
 hold off
 
 
+%}
 
 
 
@@ -211,8 +291,10 @@ hold off
 
 %ica
 
-Zica_G = fastICA(G_de,3,"kurtosis",0);%mine
-Zica_GG = fastICA(GG_de,3,"kurtosis",0);%matlab
+Zica_G = fastICA(G_de,3,"kurtosis",0); %--------run this line
+
+%{
+Zica_GG = fastICA(G_de,5,"negentropy",0);%matlab
 Zica_R = fastICA(R_de,3,"kurtosis",0);
 Zica_RR = fastICA(RR_de,3,"kurtosis",0);
 Zica_B = fastICA(B_de,3,"kurtosis",0);
@@ -223,79 +305,67 @@ ZICA_BB = fastICA(BB_de,3,"kurtosis,0",0);
 %Zica_RR = fastICA(RR_de,3);
 
 figure(19);
-plot(t,Zica_R(1,:),Color="r");
+plot(t,Zica_GG(1,:),Color="r");
 hold on
-plot(t,Zica_G(1,:),Color='g');
-plot(t,Zica_B(1,:),Color='b');
+plot(t,Zica_G(2,:),Color='g');
+plot(t,Zica_GG(5,:),Color='b');
 hold off
 
 
-figure(20);
+figure('Name','After ICA');
 plot(t,Zica_R(1,:),LineStyle="-",Color="r");
 hold on
 plot(t,Zica_G(1,:),Color='g');
-plot(t,Zica_B(3,:),LineStyle="-.",Color='b');
+plot(t,Zica_B(1,:),LineStyle="-.",Color='b');
 
 
 figure(21);
-plot(t,Zica_B(1,:),LineStyle="-",Color='r');
+plot(t,Zica_G(1,:),Color='g',LineStyle='-');
 hold on
-plot(t,Zica_GG(2,:),LineStyle=":",Color='g');
-plot(t,Zica_GG(3,:),LineStyle="-.",Color='b');
+plot(t,Zica_G(3,:),Color='r',LineStyle='--');
+plot(t,Zica_G(5,:),Color='g',LineStyle=':');
 
 
 figure(22);
-plot(t,Zica_R(3,:),LineStyle="-",Color='r');
+plot(t,Zica_GG(3,:),LineStyle="-",Color='r');
 hold on
 plot(t,Zica_GG(3,:),LineStyle=":",Color='g');
-plot(t,Zica_B(3,:),LineStyle="-.",Color='b');
+plot(t,Zica_GG(3,:),LineStyle="-.",Color='b');
+
+
+te = snr(Zica_G(1,:));
+te = snr(Zica_G(2,:));
+%}
 
 
 
 %------------Part 7: FFT and PSD------------------
 
+%{
+nnn = bandpass(Zica_G(1,:),[0.8 2],200);
 figure(3);pwelch(Zica_R(1,:));
-figure(1);pwelch(Zica_G(1,:));
+figure(1);pwelch(Zica_G(1,:)); 
 figure(2);pwelch(R_Source);
 
 
 figure(4);pwelch(normalizeG);
-[ppxg,fG] = pwelch(Zica_G(1,:));
-[ppxgg,fGG] = pwelch(Zica_GG(1,:));
+
+%}
+
+%----run this line from here
+[ppxg,fG] = pwelch(nnn);
+[ppxgg,fGG] = pwelch(Zica_G(1,:));
 
 figure('Name','signal');
-plot(fG,10*log10(ppxg));
+plot(fGG,10*log10(ppxgg));
+%--------- to there
 
 pwelch(normalizeG);
 
 
 %----------FFT OF the signal-------------
-Fs = 1000;            % Sampling frequency                    
-T = 0.001;             % Sampling period       
-L = 100;             % Length of signal
-time_vec = (0:L-1);        % Time vector
-new_time_vec =  time_vec.*T;
 
-
-Y = fft(Zica_G(1,:));
-plot(Fs/L*(0:L-1),abs(Y),"LineWidth",2)
-title("Complex Magnitude of fft Spectrum")
-xlabel("f (Hz)")
-ylabel("|fft(X)|")
-
-
-
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-
-f = Fs/L*(0:(L/2));
-plot(f,P1,"LineWidth",3) 
-title("Single-Sided Amplitude Spectrum of X(t)")
-xlabel("f (Hz)")
-ylabel("|P1(f)|")
-
-
+%{
 N =1000;
 Y = Y(1:N/2+1);
 psdx = (1/(2*pi*N)) * abs(Y).^2;
@@ -315,187 +385,31 @@ ts = linspace(0,100,10000);
 %linspace goes from 0 to pi with 100000 subdivisions
 w = 2*pi*f; %this is the angluar frequency or the fundamental frequency
 
+%}
 
-ft1 = A*sin(w*ts);
-ft2 = A*sin(2*pi*300*ts);
-ft3 = A*sin(2*pi*150*ts);
-ft4 = A*sin(2*pi*100*ts);
 
-plot(ts,ft1);
-hold on
-plot(ts,ft2);
-plot(ts,ft3);
-plot(ts,ft4);
 
-hold off
+%---------------------Bandpass filtering------------------
 
-X = ft1+ft2+ft3+ft4;
-plot(ts,X);
 
-%showing fft
-plot(ts,ft1);
-Fs= 1000; %sampling frequency
-T = 1/Fs; %sampling period
-L = 10000;
-t = (0:L-1)*T; %time vector
+figure(40);bandpass(Zica_G(1,:), [0.8 2], 200); %----run this line
 
-ft_new = fft(X);
-plot(Fs/L*(0:L-1),abs(ft_new));
-
-%----------Bandpass filtering--------
-
-Fs = 1000; % Sampling frequency (Hz)
-Fc1 = 0.8; % Lower cutoff frequency (Hz)
-Fc2 = 2; % Upper cutoff frequency (Hz)
-[b, a] = butter(4, [Fc1 Fc2]/(Fs/2)); % 4th order Butterworth filter
-
-% Apply the filter to a signal
-filtered_signal = filter(b, a, Zica_G(1,:));
-plot(filtered_signal);
-
-figure(1);bandpass(ZICA_MAF_G(1,:), [0.8 2], 300);%
-figure(2);bandpass(ZICA_MAF_R(1,:), [0.8 2], 300);%
-figure(30);bandpass(Zica_R(1,:), [0.8 2], 100);
-figure(40);bandpass(Zica_G(1,:), [0.8 2], 100);
+%{
+figure(30);bandpass(Zica_G(1,:), [0.8 2], 100);
+figure(46);bandpass(Zica_GG(1,:), [0.8 2], 200);
 figure(6);bandpass(Zica_G(2,:), [0.8 2], 500);
-
-
-
-hold off
-
-%----------PSD Of the signal---------
-
-
-%{
-%{
-for x = 1:10
-    %fprintf('hello\n');
-    %sets each frame into the respective array
-    str(x)= sprintf(" 'C:\\Users\\unity\\source\\repos\\Year two\\clas\\ECNG 3020 Heart Rate\\frame%d.jpg' ",count);
-    %disp(str(x));
-    %image_vec(x) = imread(str(x)); 
-    count= count+1;
-
-end 
-
-%testim = imread(str(1));
 %}
 
 
-
-
-
-
-%------------PART 2: RGB Channel------------------ 
-
-location = 'C:\Users\unity\source\repos\Year two\clas\ECNG 3020 Heart Rate\frame0.jpg';
-image1 = imread(location);
-image1R = image1(:,:,1);
-image1G = image1(:,:,2);
-image1B = image1(:,:,3);
-RegRowSt = 500;
-RegRowFin = 900;
-RegColSt = 500;
-RegColFin = 700;
-
-%getting the rgb colour channel
-image1GF = double(image1G);
-image1RF = double(image1R);
-image1BF = double(image1B);
-
-
-%-------------Part 3: Select a ROI---------------------
-
-%selecting the region of interest
-ROI = image1GF(RegRowSt:RegRowFin,RegColSt:RegColFin);
-ROI2 = image1RF(RegRowSt:RegRowFin,RegColSt:RegColFin);
-ROI3 = image1BF(RegRowSt:RegRowFin,RegColSt:RegColFin);
-
-
-%displaying the region of interest
-figure(1);imshow(uint8(ROI));
-figure(2);imshow(uint8(ROI2));
-figure(3);imshow(uint8(ROI3));
-%}
-
-%-------------Test Part---------------
 %{
-location = 'C:\Users\unity\source\repos\Year two\clas\ECNG 3020 Heart Rate\frame0.jpg';
-image1G = image1d(:,:,2);
-RegRowSt = 500;
-RegRowFin = 900;
-RegColSt = 500;
-RegColFin = 700;
-image1GF = double(image1G);
-ROI = image1GF(RegRowSt:RegRowFin,RegColSt:RegColFin);
-%}
-
-
-%---------------PART 4: Use Filter---------------
-%{
-%image_test =  'C:\Users\unity\source\repos\Year two\clas\ECNG 3020 Heart Rate\frame9.jpg';
-%image1 = imread(RO1);
-%image1d = double(image1);
-
-
-%%% Averaging Filter
-%Task 3: Create a 2D averaging filter of size n
-%create a variable with the filter size
-AvgFilSize = 50;
-%Create a 2D empty shell with ones with the size
-AvgFil = ones(AvgFilSize,AvgFilSize);
-% Make sure the filter sums up to one (normalization)
-AvgFil = AvgFil/sum(AvgFil(:));
-% apply the filter to the image using convolution
-ImgR = convn(double(ROI2),AvgFil,'same');
-ImgG = convn(double(ROI),AvgFil,'same');
-ImgB = convn(double(ROI3),AvgFil,'same');
-
-% show the image
-figure(7);subplot(2,2,3);imshow(uint8(ImgF)); title('ImgF-10');
-figure(7);subplot(2,2,4);imshow(uint8(ROI)); title('-original');
-
-%}
-
-%-----------PART 5: Independent Component Analysis---------------
-
-%-----------PART 5, a)signal source creation---------------
-% 
-%i am finding the mean of the image
-% i am doing this to obtain a time series of means of the frames
-%{
-meanIntensityValueR = mean2(ImgR);
-meanIntensityValueG = mean2(ImgG);
-meanIntensityValueB = mean2(ImgB);
- %}
-
-
-%{
-%comp = 2;
-%train ICA model
-y = fft2(image1GF);
-y = abs(y);
-figure(2);
-y = log(y+1);
-
-imshow(y,[]);
-title('fourier transform of an image')
-
-Mld = rica(y,2);
-
-%plot(Mld.)
-%apply ICA
-%data_ICA = transform(Mld,)
-%}
 
 
 
-%}
 %t = 0:2*pi/1000:2*pi;
 
 Fs= 1000; %sampling frequency
 T = 1/Fs; %sampling period
-L = 30000;
+L = 20000;
 t = (0:L-1)*T; %time vector
 
 y = 0.8 + 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
@@ -510,7 +424,7 @@ n = 500;
 m=1;
 
 for x=1:6
-    seg(x,:)= y(m:n);
+    seg(x,:)= y(x,m:n);
     m=n;
     n=n+n;
 end
@@ -520,27 +434,66 @@ figure("Name","full time");
 plot(t,y);
 
 %finding the pwd using the fft
-nfft = fft(Zica_G(1,:));%the fft
+nnn = bandpass(Zica_G(1,:),[0.8 2],200);
+nfft_1 = fft(nnn(1:150));%the fft
+nfft_2 = fft(nnn(151:300));%the fft
+nfft_3 = fft(nnn(301:450));%the fft
+nfft_4 = fft(nnn(451:600));%the fft
 
-plot(Fs/L*(0:600-1),abs(nfft),"LineWidth",3)
+
+
+
+plot(Fs/L*(0:600-1),abs(nfft))
 title("Complex Magnitude of fft Spectrum")
 xlabel("f (Hz)")
 ylabel("|fft(X)|")
 
-real_val = abs(nfft);%magnitudes of it
 
 %length of it
-length_nfft = length(abs(nfft));
-sq_val = (abs(nfft).^2)./(2);  %the actual PSD
-sq_val = (abs(nfft).^2)./(length(nfft));  %the actual PSD
+
+sq_val_1 = (abs(nfft_1).^2)./(length(nfft_1));  %the actual PSD
+sq_val_2 = (abs(nfft_2).^2)./(length(nfft_2));  %the actual PSD
+sq_val_3 = (abs(nfft_3).^2)./(length(nfft_3));  %the actual PSD
+sq_val_4 = (abs(nfft_4).^2)./(length(nfft_4));  %the actual PSD
+
+
 
 figure("Name","seg 1 psd");
-plot(sq_val);
+plot(Fs/L*(0:150-1),sq_val_1);
+hold on
+plot(Fs/L*(151:300),sq_val_2);
+plot(Fs/L*(301:450),sq_val_3);
+plot(Fs/L*(451:600),sq_val_4);
+hold off
 
-%{
-    % i am looking for the median peak within the 0.8 to 2Hz range
+sq = zeros(4,150);
 
-%}
+
+sq(1,:) = sq_val_1;
+sq(2,:) = sq_val_2;
+sq(3,:) = sq_val_3;
+sq(4,:) = sq_val_4;
+
+fin = median(sq);
+
+plot(Fs/L*(0:150-1),fin);
+
+
+
+
+
+
+Y = fft(Zica_G(1,:));
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+plot(f,P1,"LineWidth",3) 
+title("Single-Sided Amplitude Spectrum of S(t)")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
+
+
 bfd = bandpass(seg_1,[0.8 2],100);
 plot(t(1:500),bfd);
 st =  fft(bfd);
@@ -573,3 +526,4 @@ points = detectMinEigenFeatures(im2gray(I),"ROI",  bbox);
 figure, imshow(videoFrame), hold on, title("Detected features");
 plot(points);
 
+%}
